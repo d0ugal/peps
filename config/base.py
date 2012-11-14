b05bf2aebcbd4f895b70cf19ce41c706252d73f0
Dougal Matthews <dougal85@gmail.com>
2012-11-14 20:20:23 +0000

    Settings tidy up

--- config/base.py
+++ config/base.py
@@ -1,23 +1,11 @@
-import os
-_basedir = os.path.abspath(os.path.dirname(__file__))
-
-DEBUG = True
+from os import environ, path
+_basedir = path.abspath(path.dirname(__file__))
 
 ADMINS = frozenset(['dougal85@gmail.com'])
-SECRET_KEY = os.environ.get('SECRET_KEY', "Yikes.")
-
-SQLALCHEMY_DATABASE_URI = "postgresql://pep:pep@localhost/pep"
-DATABASE_CONNECT_OPTIONS = {}
-
-THREADS_PER_PAGE = 8
-
 CSRF_ENABLED = True
-CSRF_SESSION_KEY = os.environ.get('CSRF_SESSION_KEY')
-
-SQLALCHEMY_ECHO = True
-
-
-try:
-    from local_config import *
-except ImportError:
-    pass
+CSRF_SESSION_KEY = environ.get('CSRF_SESSION_KEY')
+DATABASE_CONNECT_OPTIONS = {}
+DEBUG = False
+SECRET_KEY = environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+THREADS_PER_PAGE = 8
