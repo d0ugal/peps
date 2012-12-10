4c19d7b4a848dcf9473d915ccc7db807c67341f8
Dougal Matthews <dougal85@gmail.com>
2012-12-10 21:12:34 +0000

    re-factor

--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
-from datetime import date, datetime
+from datetime import datetime
 from urlparse import urljoin
 
-from dateutil import rrule
 from flask import abort, request, Response, Blueprint, render_template, redirect
 from sqlalchemy.orm import undefer
 from werkzeug.contrib.atom import AtomFeed
 
 from pep.models import Pep
 from pep.tasks import sort_peps
+from pep.reading import passed_days, passed_weeks, passed_weekdays, get_reading_list
 from util.cache import cached
 
 mod = Blueprint('base', __name__)
@@ -125,33 +125,14 @@ def stats():
     )
 
 
-# Totally arbitrary date. We just need a starting point to count the number
-# of passed days from. Also THE BEGINNING OF TIME sounds cool - just make sure
-# you say it in a booming voice.
-THE_BEGINNING_OF_TIME = date(2012, 11, 29)
-
-
 @mod.route('/pep-a-<interval>.<format>')
 @cached(timeout=60 * 60 * 24)  # 1 day
 def pep_a_(interval, format):
 
-    today = date.today()
-    passed_days = (today - THE_BEGINNING_OF_TIME).days
-    passed_weeks = passed_days / 7
-
-    # Use dateutils ruleset to count the number of working days (we are
-    # definging that as mon-fri) between two dates.
-    dates = rrule.rruleset()
-    dates.rrule(rrule.rrule(rrule.DAILY, dtstart=THE_BEGINNING_OF_TIME, until=today))
-    dates.exrule(rrule.rrule(rrule.DAILY, byweekday=(rrule.SA, rrule.SU), dtstart=THE_BEGINNING_OF_TIME))
-    #This is includive of start and end dates - so to normalise and make it
-    # the same as the daily calcs, remove one.
-    passed_weekdays = dates.count() - 1
-
     intervals = {
-        'day': passed_days,
-        'week': passed_weeks,
-        'weekday': passed_weekdays,
+        'day': passed_days(),
+        'week': passed_weeks(),
+        'weekday': passed_weekdays(),
     }
 
     try:
@@ -159,29 +140,15 @@ def pep_a_(interval, format):
     except KeyError:
         abort(404)
 
-    if format != "rss" and format != "atom":
+    if format not in ["rss", "atom"]:
         abort(404)
 
-    total = Pep.query.count()
-
-    # If the metric is bigger than the number of peps, we want the remainder
-    # and we can go back to the start with that.
-    metric = metric % total
-
-    all_peps = Pep.query.order_by(Pep.number.asc()).all()
-    peps = all_peps[max(metric - 10, 0): metric]
-
-    if metric < 10 and THE_BEGINNING_OF_TIME.year != today.year:
-        peps = peps + list(reversed(all_peps[total - (10 - metric): total]))
-
+    peps = get_reading_list(metric, 10)
     title = "Read a PEP a %s" % interval
 
     if format == "rss":
-        return render_template('rss.xml',
-            now=datetime.now(),
-            title=title,
-            posts=peps,
-        )
+        now = datetime.now()
+        return render_template('rss.xml', now=now, title=title, posts=peps)
 
     feed = AtomFeed(title, feed_url=request.url, url=request.url_root)
 
@@ -190,7 +157,7 @@ def pep_a_(interval, format):
                  content_type='html',
                  author=article.properties['author'],
                  url=make_external(article.url()),
-                 updated=article.added,
+                 updated=article.updated,
                  published=article.added)
 
     return feed.get_response()
---
+++ pep/reading.py
@@ -0,0 +1,52 @@
+from datetime import date
+
+from dateutil import rrule
+
+from pep.models import Pep
+
+# Totally arbitrary date. We just need a starting point to count the number
+# of passed days from. Also THE BEGINNING OF TIME sounds cool - just make sure
+# you say it in a booming voice.
+THE_BEGINNING_OF_TIME = date(2012, 11, 29)
+
+
+def passed_days():
+    today = date.today()
+    return (today - THE_BEGINNING_OF_TIME).days
+
+
+def passed_weeks():
+    return passed_days() / 7
+
+
+def passed_weekdays():
+
+    today = date.today()
+
+    # Use dateutils ruleset to count the number of working days (we are
+    # definging that as mon-fri) between two dates.
+    dates = rrule.rruleset()
+    dates.rrule(rrule.rrule(rrule.DAILY, dtstart=THE_BEGINNING_OF_TIME, until=today))
+    dates.exrule(rrule.rrule(rrule.DAILY, byweekday=(rrule.SA, rrule.SU), dtstart=THE_BEGINNING_OF_TIME))
+    #This is includive of start and end dates - so to normalise and make it
+    # the same as the daily calcs, remove one.
+    passed_weekdays = dates.count() - 1
+
+    return passed_weekdays
+
+
+def get_reading_list(metric, count):
+
+    total = Pep.query.count()
+
+    # If the metric is bigger than the number of peps, we want the remainder
+    # and we can go back to the start with that.
+    metric = metric % total
+
+    all_peps = Pep.query.order_by(Pep.number.asc()).all()
+    peps = all_peps[max(metric - count, 0): metric]
+
+    if metric < count and THE_BEGINNING_OF_TIME.year != date.today().year:
+        peps = peps + list(reversed(all_peps[total - (count - metric): total]))
+
+    return peps
